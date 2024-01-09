ba3fd1055888203b5de703d3bdfae9db81009453
MuhamedElsafy <muhamedd.kh@gmail.com>
2024-01-09 17:49:07 +0200

    add

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { CreateUserComponent } from './components/create-user/create-user.component';
 import { ForgetPassComponent } from './components/forget-pass/forget-pass.component';
+import { AddVehicleComponent } from './components/add-vehicle/add-vehicle.component';
 
 const routes: Routes = [
   {path:'Login',component:LoginComponent},
   {path:'createUser',component:CreateUserComponent},
   {path:'ForgetPass',component:ForgetPassComponent},
+  {path:'addVehicle',component:AddVehicleComponent},
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { CreateUserComponent } from './components/create-user/create-user.component';
 import { ForgetPassComponent } from './components/forget-pass/forget-pass.component';
+import { AddVehicleComponent } from './components/add-vehicle/add-vehicle.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +14,8 @@ import { ForgetPassComponent } from './components/forget-pass/forget-pass.compon
     LoginComponent,
     NavbarComponent,
     CreateUserComponent,
-    ForgetPassComponent
+    ForgetPassComponent,
+    AddVehicleComponent
   ],
   imports: [
     BrowserModule,
