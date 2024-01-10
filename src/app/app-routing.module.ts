517a1640a1f93f3a5f90372382688f5a645d10ca
MuhamedElsafy <muhamedd.kh@gmail.com>
2024-01-10 18:32:49 +0200

    sall and out done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { LoginComponent } from './components/login/login.component';
 import { CreateUserComponent } from './components/create-user/create-user.component';
 import { ForgetPassComponent } from './components/forget-pass/forget-pass.component';
 import { AddVehicleComponent } from './components/add-vehicle/add-vehicle.component';
-
 import { HomeComponent } from './components/home/home.component';
 import { SearchComponent } from './components/search/search.component';
 import { NotfoundComponent } from './components/notfound/notfound.component';
 import { VehicleDetailsComponent } from './components/vehicle-details/vehicle-details.component';
 import { authGuard } from './gardes/auth.guard';
+import { VehiclesComponent } from './vehicles/vehicles.component';
+import { SaleVehiclesComponent } from './sale-vehicles/sale-vehicles.component';
+import { VehiclesOutComponent } from './vehicles-out/vehicles-out.component';
 
 
 const routes: Routes = [
@@ -21,7 +23,9 @@ const routes: Routes = [
   {path:'addVehicle',component:AddVehicleComponent},
   {path:'search',component:SearchComponent},
   {path:"VehicleDetails/:Id",component:VehicleDetailsComponent},
-  {path:"addVehicle",component:AddVehicleComponent},
+  {path:"vehicles",component:VehiclesComponent},
+  {path:'saleVehicles',component:SaleVehiclesComponent},
+  {path:'vehiclesOut',component:VehiclesOutComponent},
   {path:'**',component:NotfoundComponent},
 
   ];
---
+++ src/app/sale-vehicles/sale-vehicles.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sale-vehicles',
+  templateUrl: './sale-vehicles.component.html',
+  styleUrl: './sale-vehicles.component.css'
+})
+export class SaleVehiclesComponent {
+
+}
